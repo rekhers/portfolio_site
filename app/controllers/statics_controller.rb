76abcf4309e6915afb5818859c63003202724f30
Rekha Tenjarla <rekha.tenjarla@gmail.com>
2015-01-12 20:24:36 -0600

    init

---
+++ app/controllers/statics_controller.rb
@@ -0,0 +1,19 @@
+class StaticsController < ApplicationController
+  
+  def about 
+    download
+  end
+  
+  def resume
+  end
+  
+  def download 
+    html = render_to_string(:action => :show, :file => "/Users/rekhatenjarla/Desktop/MAIN-res/app/views/statics/about.html.erb") 
+    pdf = WickedPdf.new.pdf_from_string(html) 
+    send_data(pdf, 
+      :filename    => "my_pdf_name.pdf", 
+      :disposition => 'attachment') 
+  end
+   
+   
+end
